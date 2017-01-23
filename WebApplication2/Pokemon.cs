0ee41173ba92935fa246c44b84c7d2c06020dc16
Ravi Chande <rchande@microsoft.com>
2017-01-23 11:09:56 -0800

    Add project files.

---
+++ WebApplication2/Pokemon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Moves { get; set; }
+    }
+}
